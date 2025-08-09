bb82cf37363bf14b5172241a409bd9f8ccc37fb0
Felipe Mendes Neves <felipemen74@gmail.com>
2025-08-08 21:37:47 -0300

    feat: adiciona api cadastro de usuario e visualiza usuarios

---
+++ app/Http/Controllers/API/V1/UserController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers\API\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\UserRequest;
+use App\Models\User;
+use Exception;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\DB;
+
+class UserController extends Controller
+{
+    public function index(): JsonResponse {
+
+        try {
+            $users = User::all();
+            return response()->json([
+                'version' => 'v1',
+                'status' => true,
+                'users' => $users,
+            ], 200);
+        } catch (Exception $e) {
+            return response()->json([
+                'error' => 'Erro ao buscar usuários',
+                'message' => $e->getMessage()
+            ], 500);
+        }
+        
+    }
+
+    public function show(User $user): JsonResponse {
+        try {
+            return response()->json([
+                'version' => 'v1',
+                'status' => true,
+                'user' => $user
+            ], 200);
+        } catch (Exception $e) {
+            return response()->json([
+                'error' => 'Erro ao buscar usuário',
+                'message' => $e->getMessage()
+            ], 500);
+        }
+    }
+
+    public function store(UserRequest $request) {
+        DB::beginTransaction();
+        try {
+            $user = User::create($request->all());
+            DB::commit();
+            return response()->json([
+                'version' => 'v1',
+                'status' => true,
+                'user' => $user,
+                'message' => 'Usuário criado com sucesso'
+            ], 201);
+        } catch (Exception $e) {
+            DB::rollBack();
+            return response()->json([
+                'error' => 'Erro ao criar usuário',
+                'message' => $e->getMessage()
+            ], 400);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,12 @@
 <?php
 
+use App\Http\Controllers\API\V1\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
+Route::prefix('v1')->group(function () {
+   Route::get('/users', [UserController::class, 'index']); 
+   Route::get('/users/{user}', [UserController::class, 'show']);
+   Route::post('/users', [UserController::class, 'store']);
+});
